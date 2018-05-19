feb232f6d04e8ad9c6266a7cc7de7b71caec4992
Sascha Grunert <mail@saschagrunert.de>
2018-05-19 12:10:44 +0200

    Fix build

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![deny(missing_docs)]
 #![deny(warnings)]
 #![no_std]
 
@@ -6,5 +5,4 @@ extern crate bare_metal;
 extern crate cortex_m;
 extern crate vcell;
 
-/// The system view description
 pub mod svd;
