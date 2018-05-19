92bf8a8a2df68c996e713687f4b3139bde6c1eb1
Sascha Grunert <mail@saschagrunert.de>
2018-05-19 12:02:30 +0200

    Improve layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
-#![deny(missing_docs)]
-#![deny(warnings)]
-#![allow(non_camel_case_types)]
 #![no_std]
 
 extern crate bare_metal;
 extern crate cortex_m;
-#[cfg(feature = "rt")]
-extern crate cortex_m_rt;
 extern crate vcell;
 
 pub mod svd;
